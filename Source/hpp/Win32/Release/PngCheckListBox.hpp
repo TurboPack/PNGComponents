115e234f4ba86c3fe4babc6a4b3cbc735ed3a118
Roman Kassebaum <roman.kassebaum@kdv-dt.de>
2015-08-22 02:09:34 +0200

    Compiled with the latest Delphi version.

--- Source/hpp/Win32/Release/PngCheckListBox.hpp
+++ Source/hpp/Win32/Release/PngCheckListBox.hpp
@@ -2,7 +2,7 @@
 // Copyright (c) 1995, 2015 by Embarcadero Technologies, Inc.
 // All rights reserved
 
-// (DO NOT EDIT: machine generated header) 'PngCheckListBox.pas' rev: 29.00 (Windows)
+// (DO NOT EDIT: machine generated header) 'PngCheckListBox.pas' rev: 30.00 (Windows)
 
 #ifndef PngchecklistboxHPP
 #define PngchecklistboxHPP
--- Source/hpp/Win64/Release/PngBitBtn.hpp
+++ Source/hpp/Win64/Release/PngBitBtn.hpp
@@ -2,7 +2,7 @@
 // Copyright (c) 1995, 2015 by Embarcadero Technologies, Inc.
 // All rights reserved
 
-// (DO NOT EDIT: machine generated header) 'PngBitBtn.pas' rev: 29.00 (Windows)
+// (DO NOT EDIT: machine generated header) 'PngBitBtn.pas' rev: 30.00 (Windows)
 
 #ifndef PngbitbtnHPP
 #define PngbitbtnHPP
